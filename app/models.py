aec410a3fc95daacd4f4b5a43367d323cde0679f
tharcissie <tharcissieidufashe@gmail.com>
2020-12-22 19:12:40 +0200

    user in post

--- app/models.py
+++ app/models.py
@@ -5,11 +5,24 @@ from django.dispatch import receiver
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    caption = models.CharField(max_length=700, blank=True)
+    date = models.DateField(auto_now_add=True, blank=True)
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
     caption = models.CharField(max_length=250, blank=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
 
     class Meta:
         ordering = ["-pk"]
@@ -24,15 +37,5 @@ class Post(models.Model):
         return self.name
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    caption = models.CharField(max_length=700, blank=True)
-    date = models.DateField(auto_now_add=True, blank=True)
 
-@receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
 

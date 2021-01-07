499d1816fbcea694dc484bd3a0290b4a71ad3ea3
tharcissie <tharcissieidufashe@gmail.com>
2021-01-07 19:41:06 +0200

    test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile, Post
+from django.contrib.auth.models import User
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='tharcissie')
+        self.user.save()
+
+        self.profile_test = Profile(id=1, name='image', profile_picture='a.jpg', bio='bio',
+                                    user=self.user)
+
+
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='tharcissie', user=User(username='idufashe'))
+        self.profile_test.save()
+
+        self.image_test = Post(image='a.png', name='test', caption='default test', user=self.profile_test)
+
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
